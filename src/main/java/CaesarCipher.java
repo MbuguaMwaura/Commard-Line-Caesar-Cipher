47dfd8d521cf960aff86be2778cb8a8e671d307c
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-10 15:51:22 +0300

    adds tests to read the key and first change the string without the key

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,12 +1,20 @@
 public class CaesarCipher{
 
     private String mStatement;
-
+    private int mKey;
 
     public CaesarCipher(String statement, int key){
         mStatement = statement;
+        mKey = key;
     }
     public String getStatement(){
         return mStatement ;
+    }
+    public int getKey(){
+        return mKey;
+    }
+    public String isEncrypted(){
+        mStatement = "b";
+        return mStatement;
     }
 }
--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -14,4 +14,16 @@ public class CaesarCipherTest{
         CaesarCipher testCaesarCipher = new CaesarCipher("a",2);
         assertEquals("a",testCaesarCipher.getStatement());
     }
+
+    @Test
+    public void newCaesarCipher_readKey_2(){
+        CaesarCipher testCaesarCipher = new CaesarCipher("a",2);
+        assertEquals(2,testCaesarCipher.getKey());
+    }
+
+    @Test
+    public void newCaesarCipher_replaceLetterUsingKey_b(){
+        CaesarCipher testCaesarCipher = new CaesarCipher("a",2);
+        assertEquals("b",testCaesarCipher.isEncrypted());
+    }
 }
