d70c5bc85eca24916f0d8a870ee3a2c57fcc240d
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-10 15:35:55 +0300

    tests whether a stringcan be read

--- src/main/java/CaesarCipher.java
+++ src/main/java/CaesarCipher.java
@@ -1,3 +1,12 @@
 public class CaesarCipher{
 
+    private String mStatement;
+
+
+    public CaesarCipher(String statement, int key){
+        mStatement = statement;
+    }
+    public String getStatement(){
+        return mStatement ;
+    }
 }
--- src/test/java/CaesarCipherTest.java
+++ src/test/java/CaesarCipherTest.java
@@ -5,8 +5,13 @@ public class CaesarCipherTest{
 
     @Test
     public void newCaesarCipher_instantiateCorrectly(){
-        CaesarCipher testCaesarCipher = new CaesarCipher();
+        CaesarCipher testCaesarCipher = new CaesarCipher("a",2);
         assertEquals(true,testCaesarCipher instanceof CaesarCipher);
     }
 
+    @Test
+    public void newCaesarCipher_readsAString_a(){
+        CaesarCipher testCaesarCipher = new CaesarCipher("a",2);
+        assertEquals("a",testCaesarCipher.getStatement());
+    }
 }
