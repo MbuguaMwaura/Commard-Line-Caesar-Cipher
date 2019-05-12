c04bb7beeb16791048e390297d16df789f01bb50
Mbugua Mwaura <mbuguamwaura9@gmail.com>
2019-05-12 22:12:55 +0300

    adds the ability to exit the program

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,41 +1,46 @@
 import java.io.Console;
 import java.util.Scanner;
 
-public class App{
-    public static void main(String[] args){
+public class App {
+    public static void main(String[] args) {
         Console myConsole = System.console();
         Scanner myScanner = new Scanner(System.in);
 
+        boolean runProgram = true;
 
-        System.out.println("Welcome to a Caesar Ciphar program. Would you like to encrypt or decrypt a statement?");
-        String userAnswer = myConsole.readLine();
+        while (runProgram) {
+            System.out.println("Welcome to a Caesar Ciphar program. Would you like to encrypt or decrypt a statement or would you like to exit?");
+            String userAnswer = myConsole.readLine();
 
 
-if(userAnswer.equals("encrypt")) {
-    System.out.println("Please enter a statement that you would like to encrypt:");
-    String statement = myConsole.readLine();
+            if (userAnswer.equals("encrypt")) {
+                System.out.println("Please enter a statement that you would like to encrypt:");
+                String statement = myConsole.readLine();
 
-    System.out.println("Please enter an encryption key");
-    int key = myScanner.nextInt();
+                System.out.println("Please enter an encryption key");
+                int key = myScanner.nextInt();
 
-    CaesarCipher userEncryption = new CaesarCipher(statement, key);
-    String encryption = userEncryption.isActuallyEncrypted(statement, key);
+                CaesarCipher userEncryption = new CaesarCipher(statement, key);
+                String encryption = userEncryption.isActuallyEncrypted(statement, key);
 
-    System.out.println("The encrypted statement is: " + encryption);
-}
-else if (userAnswer.equals("decrypt")) {
-    System.out.println("Please enter a statement to decrypt: ");
-    String statementDecrypt = myConsole.readLine();
+                System.out.println("The encrypted statement is: " + encryption);
+            } else if (userAnswer.equals("decrypt")) {
+                System.out.println("Please enter a statement to decrypt: ");
+                String statementDecrypt = myConsole.readLine();
 
-    System.out.println("Please enter an decryption key");
-    int keyDecrypt = myScanner.nextInt();
+                System.out.println("Please enter an decryption key");
+                int keyDecrypt = myScanner.nextInt();
 
-    CaesarCipher userDecryption = new CaesarCipher(statementDecrypt, keyDecrypt);
-    String decryption = userDecryption.toDecrypt(statementDecrypt, keyDecrypt);
+                CaesarCipher userDecryption = new CaesarCipher(statementDecrypt, keyDecrypt);
+                String decryption = userDecryption.toDecrypt(statementDecrypt, keyDecrypt);
 
-    System.out.println("The decrypted statement is: " + decryption);
-}else {
-    System.out.println("Sorry we could not recognise your input");
-}
+                System.out.println("The decrypted statement is: " + decryption);
+            } else if (userAnswer.equals("exit")){
+                runProgram = false;
+            }
+            else {
+                System.out.println("Sorry we could not recognise your input");
+            }
+        }
     }
 }
